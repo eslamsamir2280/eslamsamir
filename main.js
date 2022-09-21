9af81fa7bf02b943a2362f001e7674aaeab70a8e
eslam samir <88991029+eslamsamir2280@users.noreply.github.com>
2022-09-21 15:49:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector(".bx-menu");
+let btnclose = document.querySelector(".bx-x");
+let head = document.querySelector(".header");
+
+const toggelnav = () => {
+  head.classList.toggle("active");
+};
+btn.addEventListener("click", () => toggelnav());
+btnclose.addEventListener("click", () => toggelnav());
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".text , .image ", { delay: 300, origin: "top" });
+sr.reveal(".about-text", { delay: 100, origin: "left" });
+sr.reveal(".about-img", { delay: 100, origin: "right" });
+sr.reveal(".skill-bars", { delay: 100, origin: "bottom" });
+sr.reveal(".logos--img", { interval: 100 });
+sr.reveal(".skills-img", { origin: "top", delay: 100 });
+sr.reveal(".value--content , .contact--images", { origin: "right" });
